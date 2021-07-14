d18ed68f0bbb4bc0c0c76d298c6e910f64aa1a75
Kubariev Ilya <polcrazpolcraz@gmail.com>
2021-07-15 01:04:51 +0300

    1:58:41

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,204 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="app">
+    <h1>Страница с постами</h1>
+    <my-input
+      v-model="searchQuery"
+      placeholder="Поиск..."
+    />
+    <div class="app__btns">
+      <my-button
+          @click="showDialog"
+      >
+        Создать пост
+      </my-button>
+
+      <my-select
+        v-model="selectedSort"
+        :options="sortOptions"
+      />
+    </div>
+
+    <my-dialog v-model:show="dialogVisible">
+      <post-form
+          @create="createPost"
+      />
+    </my-dialog>
+    <post-list
+        :posts="sortedAndSearchedPosts"
+        @remove="removePost"
+        v-if="!isPostsLoading"
+    />
+    <div v-else>Идет загрузка...</div>
+    <div ref="observer" class="observer"></div>
+<!--    <div class="page__wrapper">-->
+<!--      <div-->
+<!--          v-for="pageNumber in totalPages"-->
+<!--          :key="pageNumber"-->
+<!--          class="page"-->
+<!--          :class="{-->
+<!--            'current-page': page === pageNumber-->
+<!--          }"-->
+<!--          @click="changePage(pageNumber)"-->
+<!--      >-->
+<!--        {{ pageNumber }}-->
+<!--      </div>-->
+<!--    </div>-->
   </div>
-  <router-view/>
 </template>
 
+<script>
+import PostForm from "@/components/PostForm";
+import PostList from "@/components/PostList";
+import MyDialog from "@/components/UI/MyDialog";
+import axios from 'axios'
+
+export default {
+  components: {
+    MyDialog,
+    PostForm,
+    PostList,
+  },
+  data() {
+    return {
+      posts: [],
+      dialogVisible: false,
+      isPostsLoading: false,
+      selectedSort: '',
+      sortOptions: [
+        {value: 'title', name: 'По названию'},
+        {value: 'body', name: 'По содержимому'},
+      ],
+      searchQuery: '',
+      page: 1,
+      limit: 10,
+      totalPages: 0
+    }
+  },
+  methods: {
+    createPost(post) {
+      this.posts.push(post)
+      this.dialogVisible = false
+    },
+    removePost(post) {
+      this.posts = this.posts.filter(p => p.id !== post.id)
+    },
+    showDialog() {
+      this.dialogVisible = true
+    },
+    // changePage(pageNumber) {
+    //   this.page = pageNumber
+    // },
+    async fetchPosts() {
+      try {
+        this.isPostsLoading = true
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+          params: {
+            _page: this.page,
+            _limit: this.limit
+          }
+        })
+        this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit)
+        this.posts = response.data
+      } catch (e) {
+        alert('Ошибка')
+      } finally {
+        this.isPostsLoading = false
+      }
+    },
+    async loadMorePosts() {
+      try {
+        this.page++
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+          params: {
+            _page: this.page,
+            _limit: this.limit
+          }
+        })
+        this.posts = [...this.posts, ...response.data]
+      } catch (e) {
+        alert('Ошибка')
+      } finally {
+        this.isPostsLoading = false
+      }
+    }
+  },
+  mounted() {
+    this.fetchPosts()
+    const options = {
+      rootMargin: '0px',
+      threshold: 1.0
+    }
+
+    const callback = (entries, observer) => {
+      if(entries[0].isIntersecting && this.page < this.totalPages) {
+        this.loadMorePosts()
+      }
+    }
+
+    const observer = new IntersectionObserver(callback, options)
+    observer.observe(this.$refs.observer)
+  },
+  watch: {
+    // page() {
+    //   this.fetchPosts()
+    // },
+  },
+  computed: {
+    sortedPosts() {
+      return [...this.posts].sort((post1, post2) =>
+          post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]))
+    },
+    sortedAndSearchedPosts() {
+      return this.sortedPosts.filter(post => post.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
+    }
+  },
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Roboto', sans-serif;
 }
 
-#nav {
-  padding: 30px;
+.app {
+  padding: 20px;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.app__btns {
+  margin: 15px 0;
+  display: flex;
+  justify-content: space-between;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.page__wrapper {
+  display: flex;
+  margin-top: 15px;
+}
+
+.page {
+  border: 1px solid teal;
+  color: teal;
+  padding: 10px;
+  margin: 0 5px;
+}
+
+.page:hover {
+  cursor: pointer;
+  background: teal;
+  color: white;
+}
+
+.current-page {
+  border: 2px solid teal;
+  background: teal;
+  color: white;
+}
+
+.observer {
+  height: 30px;
 }
 </style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,53 @@
+<template>
+  <div v-if="posts.length > 0">
+    <h3>Список постов</h3>
+    <transition-group name="post-list">
+      <post-item
+          v-for="post in posts"
+          :post="post"
+          :key="post.id"
+          @remove="$emit('remove', post)"
+      />
+    </transition-group>
+  </div>
+  <div v-else>
+    <h2 style="color: red">
+      Список постов пуст
+    </h2>
+  </div>
+</template>
+
+<script>
+import PostItem from "@/components/PostItem";
+export default {
+  components: {PostItem},
+  props: {
+    posts: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.post-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all .4s ease;
+}
+
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateX(130px);
+}
+
+.post-list-move {
+  transition: transform .4s ease;
+}
+</style>
